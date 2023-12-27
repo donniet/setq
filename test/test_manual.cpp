e850906397df6c6f2f2ec2a9b95ee710e8337c22
donnie <donniet@gmail.com>
2023-12-27 11:56:53 -1000

    got a test mode with no args

--- test/test_manual.cpp
+++ test/test_manual.cpp
@@ -2,15 +2,29 @@
 #include "manual.hpp"
 
 #include <iostream>
+#include <string>
 
 using std::cin;
 using std::cout;
+using std::string;
+
 
 int main(int ac, char * av[]) {
     seqt s;
 
-    while(cin) {
-        s.read((uint32_t)cin.get());
+    if(ac > 1) { 
+        while(cin) {
+            uint32_t c = (uint32_t)cin.get();
+            if(c < 0x10000)
+                s.read(c);
+        }
+    } else {
+        // test mode
+        string test = "blah";
+
+        for(char c : test) {
+            s.read(c);
+        }
     }
 
     s.dump(cout);
